9775ba5fc080b37da09d9385e2e91f53f01fc770
ahareem <60136524+ahareem@users.noreply.github.com>
2022-02-24 11:46:31 +0000

    Update GRPCClientService.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -42,7 +42,7 @@ public class GRPCClientService {
 			.setB11(6)
 			.build());
 // 		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
-	    	String resp= A.getC00()+" "+A.getC01()+""+A.getC10()+" "+A.getC11()+"\n";
+	    	String resp=A.getC00()+a.getC01()+a.getC10()+a.getC11()+"";
 		return resp;
     }
     public String mult(){
